e03769175f3caaad5768a5e3ab81eb07546298c9
Eric Roberts <eric.dan.roberts@gmail.com>
2016-07-01 18:54:30 -0700

    9.7 Account

---
+++ 9.7 Account/src/account.java
@@ -0,0 +1,31 @@
+/** Program:  account
+  * File:     account.java 
+  * Summary:  create a class for Account
+  * Author:   Eric Roberts
+  * Date:     June 30, 2016
+**/
+
+public class account {
+
+	public static void main(String[] args) {
+		//create account
+		accountClass account = new accountClass(1122, 20000);
+		
+		//deposit
+		account.deposite(3000);
+		
+		//interest
+		account.setAnnualInterestRate(4.5);
+		
+		//withdraw
+		account.withdraw(2500);
+		
+		//print account details
+		System.out.println("Account ID: " + account.getId());
+		System.out.println("Date: " + account.getDateCreated());
+		System.out.printf("Monthly interest: $%.2f", account.getMonthlyInterest());
+		System.out.printf("Balance $%.2f", account.getBalance());
+
+	}
+
+}
---
+++ 9.7 Account/src/accountClass.java
@@ -0,0 +1,69 @@
+/** Program:  account
+  * File:     accountClass.java 
+  * Summary:  create a class for Account
+  * Author:   Eric Roberts
+  * Date:     June 30, 2016
+**/
+import java.util.Date;
+
+public class accountClass {
+	//private fields
+	private int id;
+	private double balance;
+	private static double annualInterestRate;
+	private Date dateCreated;
+	
+	//constructors for class
+	accountClass() {
+		id = 0;
+		balance = 0;
+		dateCreated = new Date();
+	}
+	
+	accountClass(int newId, double newBalance) {
+		id = newId;
+		balance = newBalance;
+		dateCreated = new Date();
+	}
+	//mutator methods
+	public void setId(int newId) {
+		id = newId;
+	}
+	
+	public void setBalance(double newBalance){
+		balance = newBalance;
+	}
+	
+	public void setAnnualInterestRate(double newAnnualInterestRate) {
+		annualInterestRate = newAnnualInterestRate;
+	}
+	
+	//accessor methods
+	public int getId() {
+		return id;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+	
+	public String getDateCreated() {
+		return dateCreated.toString();
+	}
+	
+	//regular methods
+	public double getMonthlyInterest() {
+		return balance * (getMonthlyInterest() / 100);
+	}
+	
+	public void deposite(double amount) {
+		balance += amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+}
